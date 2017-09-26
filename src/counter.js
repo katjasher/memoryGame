ce7fc97212e27f33ea78818f8373861bbf7f4083
Ekaterina Shershneva <ekatei@Ekaterinas-MacBook-Air.local>
2017-09-26 15:55:46 +0200

    change photos array

---
+++ src/counter.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+	class Counter extends React.Component {
+
+		state = {
+			count: 10
+		}
+
+		handleButtonClick1 = () => {
+			this.setState({ count: this.state.count + 1})
+
+		}
+
+		handleButtonClick2 = () => {
+			this.setState({count: this.state.count - 1})
+		}
+
+		render(){
+			return(
+				<div>
+				Count: {this.state.count}
+				<button onClick={this.handleButtonClick1}>Add!</button>
+				<button onClick={this.handleButtonClick2}>Subtract!</button>
+				</div>
+				)
+		}
+
+	}
+	export default Counter
--- src/game.js
+++ src/game.js
@@ -1,32 +1,67 @@
 import React from "react"
 import Card from "./card"
+import Counter from "./counter"
 
-const Game = () => (
+const pictures = [
+		"/images/Budapest1.jpg",
+		"/images/Budapest2.jpg",
+		"/images/Budapest3.jpg",
+		"/images/Budapest4.jpg",
+		"/images/Budapest5.jpg",
+		"/images/Budapest6.jpg"
+		/*"/images/backside1.png"*/
+
+]
+class Game extends React.Component{
+	
+	constructor(props){
+		super(props)
+		this.state = {
+			cards: this.setupGame()
+	}
+
+	}
+	
+	setupGame = () => {
+		return pictures.map (
+			(photo) => ({
+				src: photo,
+				isFlipped: false
+			}))
+	}
+
+
+	render () {
+		return (
+		<div className="game">
+		<h1 className="header"> Come and play a memory game with me!</h1>
+		{this.state.cards.map((card) => (<Card src={card.src} />) )}
+		</div>
+		)
+
+	}
+
+	}
+
+
+/*const Game = () => (
 	<div className="game">
-		<h1 className="header"> Play a memory game with me!</h1>	
+		<h1 className="header"> Play a memory game with me!</h1>
 		<Card src="/images/Budapest1.jpg" alt="" />
 		<Card src="/images/Budapest2.jpg" alt="" />
 		<Card src="/images/Budapest3.jpg" alt="" />
 		<Card src="/images/Budapest4.jpg" alt="" />
 		<Card src="/images/Budapest5.jpg" alt="" />
 		<Card src="/images/Budapest6.jpg" alt="" />
-		<Card src="/images/Budapest7.jpg" alt="" />
-		<Card src="/images/Budapest8.jpg" alt="" />
-		<Card src="/images/Budapest9.png" alt="" />
-		<Card src="/images/Budapest10.jpg" alt="" />
 		<Card src="/images/Budapest1.jpg" alt="" />
 		<Card src="/images/Budapest2.jpg" alt="" />
 		<Card src="/images/Budapest3.jpg" alt="" />
 		<Card src="/images/Budapest4.jpg" alt="" />
 		<Card src="/images/Budapest5.jpg" alt="" />
 		<Card src="/images/Budapest6.jpg" alt="" />
-		<Card src="/images/Budapest7.jpg" alt="" />
-		<Card src="/images/Budapest8.jpg" alt="" />
-		<Card src="/images/Budapest9.png" alt="" />
-		<Card src="/images/Budapest10.jpg" alt="" />
-		<Card src="/images/backside1.png" alt="" />	
+		<Card src="/images/backside1.png" alt="" />
 
 	</div>
-)
+)*/
 
 export default Game
